54a52e8ec9b824e08e9730034f98e6b48599e503
Shehryar <shehryarkashan@gmail.com>
2024-05-25 14:29:57 +0500

    change

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   pingTimeout: 60000,
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL as string],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
